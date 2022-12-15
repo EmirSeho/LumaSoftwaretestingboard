9b6be7a750fe39941b53c05dfb3400e6a6f32215
Emir Seho <sehoemir@gmail.com>
2022-12-15 11:51:21 +0100

    create new branch

---
+++ src/main/java/PageObjects/MenJacketsCatalogue.java
@@ -0,0 +1,52 @@
+package PageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import EmirSeho.AbstractComponents.AbstractComponent;
+
+
+
+public class MenJacketsCatalogue extends AbstractComponent{
+
+	WebDriver driver;
+		
+	public MenJacketsCatalogue(WebDriver driver)
+	{
+		super(driver);
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(css = ".item.product.product-item")
+	List<WebElement> products;
+	
+	By addToCart = By.cssSelector("div strong a");
+	
+	public List<WebElement> getProductList() {
+		return products;
+	}
+	
+	public WebElement getProductByName(String productName)
+	{
+		WebElement prod = getProductList().stream().filter(product->product.findElement(By.cssSelector("div strong a")).getText().equals(productName)).findFirst().orElse(null);
+		return prod;
+	}
+	
+	public void addProductToCart(String productName) throws InterruptedException
+	{
+		WebElement prod = getProductByName(productName);
+		prod.findElement(addToCart).click();
+	}
+	
+	
+	
+	
+	
+	
+}
